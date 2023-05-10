cfbf7cde8df281a99ac11110688df856c1e61630
JEREMY BOURDILLAT <bourdije@ad.u-ga.fr>
2023-05-10 16:39:53 +0200

    readme PYTHON + Spacy

--- python/FileProcessing/Processors/Spacy.py
+++ python/FileProcessing/Processors/Spacy.py
@@ -53,8 +53,8 @@ class Spacy(
 
     def tag(self, document: Document.TokenizedDocument) -> Document.TaggedDocument:
 
-
-        doc = self.nlp(self.spacyDoc(self.nlp.vocab, document.token_forms))
+        with self.nlp.select_pipes(enable=["tok2vec", "morphologizer"]):
+            doc = self.nlp(self.spacyDoc(self.nlp.vocab, document.token_forms))
 
         args = {'lemma_pos' : [i.pos_ if i.pos_ not in {"", "\n", "\r", "\t", "\f"} else "X" for i in doc]}
 
@@ -62,7 +62,8 @@ class Spacy(
 
     def lemmatize(self, document: Document.TaggedDocument) -> Document.LemmatizedDocument:
 
-        doc = self.nlp(self.spacyDoc(self.nlp.vocab, document.token_forms))
+        with self.nlp.select_pipes(enable=["lemmatizer"]):
+            doc = self.nlp(self.spacyDoc(self.nlp.vocab, document.token_forms, pos=document.lemma_pos))
 
         args = {'lemma_forms' : [i.lemma_ for i in doc]}
 
@@ -70,7 +71,8 @@ class Spacy(
     
     def deprel(self, document: Document.LemmatizedDocument) -> Document:
         
-        doc = self.nlp(self.spacyDoc(self.nlp.vocab, document.token_forms))
+        with self.nlp.select_pipes(enable=["tok2vec", "parser"]):
+            doc = self.nlp(self.spacyDoc(self.nlp.vocab, document.token_forms, pos=document.lemma_pos, lemmas=document.lemma_forms))
 
         args = {'deprels' : [i.dep_ if i.dep_ != "" else "dep" for i in doc],
                 'heads' : [document.token_sent_inds[tok.head.i] if document.token_sent_inds[tok.head.i] != document.token_sent_inds[tok.i] else 0 for tok in doc]}
